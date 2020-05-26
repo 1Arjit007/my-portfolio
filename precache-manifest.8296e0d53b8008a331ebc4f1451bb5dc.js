7acbdd6e3088c7371ef3f91fcf0963af3c5f10fe
Arjit <1arjit.kesarwani@gmail.com>
2020-05-26 16:27:10 +0530

    Updates

---
+++ precache-manifest.8296e0d53b8008a331ebc4f1451bb5dc.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e6a3f7ea0de74f512913c79c873c6e0",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "5d9573d732b81e47a6cd",
+    "url": "/my-portfolio/static/css/main.15e860bb.chunk.css"
+  },
+  {
+    "revision": "bd20af8ad516a73a61aa",
+    "url": "/my-portfolio/static/js/2.847d50d6.chunk.js"
+  },
+  {
+    "revision": "e46fc7d39878ee64e7b083a156855138",
+    "url": "/my-portfolio/static/js/2.847d50d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d9573d732b81e47a6cd",
+    "url": "/my-portfolio/static/js/main.f8fa9324.chunk.js"
+  },
+  {
+    "revision": "feb14810df33d645b05c",
+    "url": "/my-portfolio/static/js/runtime-main.9efc66b9.js"
+  },
+  {
+    "revision": "817ed7b529d245f035f2d6911a7d4991",
+    "url": "/my-portfolio/static/media/IMG-20170827-WA0008.817ed7b5.jpg"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/my-portfolio/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/my-portfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/my-portfolio/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "6d33c78f19cabffcb528b19b4e72f242",
+    "url": "/my-portfolio/static/media/hackerearth-icon.6d33c78f.png"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/my-portfolio/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "e279798296c415e8028b9c7abf8e739a",
+    "url": "/my-portfolio/static/media/project1.e2797982.png"
+  },
+  {
+    "revision": "c49a7d3dbf08ea0849677f22a9371667",
+    "url": "/my-portfolio/static/media/project2.c49a7d3d.png"
+  },
+  {
+    "revision": "fd46269aff23a0a186c48aa452d5696f",
+    "url": "/my-portfolio/static/media/project3.fd46269a.png"
+  },
+  {
+    "revision": "1a6c4c21d8336bb951d28033343cc392",
+    "url": "/my-portfolio/static/media/project4.1a6c4c21.png"
+  },
+  {
+    "revision": "e65624e91d6f66e8cdbb5dbebd6528e1",
+    "url": "/my-portfolio/static/media/project5.e65624e9.png"
+  },
+  {
+    "revision": "e815832f9bee6653735cff9778652784",
+    "url": "/my-portfolio/static/media/project6.e815832f.png"
+  },
+  {
+    "revision": "ba7d598cc8d62a732f46ae6479f806a8",
+    "url": "/my-portfolio/static/media/project7.ba7d598c.png"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/my-portfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
+  }
+]);
